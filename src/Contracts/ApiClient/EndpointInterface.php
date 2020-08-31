02100e68623df1931efe3d2be628e0a8b2d0d767
slytherincz <slytherincz@gmail.com>
2020-08-31 10:08:42 +0200

    added optional query parameters to read endpoints

--- src/Contracts/ApiClient/EndpointInterface.php
+++ src/Contracts/ApiClient/EndpointInterface.php
@@ -4,9 +4,9 @@ namespace SlytherinCz\Contracts\ApiClient;
 
 interface EndpointInterface
 {
-    public function list(?int $page = null, ?int $perPage = null): ListInterface;
+    public function list(?int $page = null, ?int $perPage = null, ?array $query = null): ListInterface;
     public function setId(string $id);
-    public function show(): DataObjectInterface;
+    public function show(?array $query = null): DataObjectInterface;
     public function create(DataObjectInterface $data): DataObjectInterface ;
     public function update(DataObjectInterface $data): DataObjectInterface;
     public function delete(): void;
